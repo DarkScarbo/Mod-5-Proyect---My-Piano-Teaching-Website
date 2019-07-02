7b1daa4fd33db9769d8fc3af34fd76b7fb123c91
github username <github email address>
2019-07-02 15:37:10 +0100

    fetch in MySpace component working

--- front-end/src/components/my_space.js
+++ front-end/src/components/my_space.js
@@ -8,13 +8,27 @@ class MySpace extends React.Component {
     messages: [],
     bookings: [],
     videos: [],
-    reviews: []
+    reviews: [],
+    students: []
   };
 
   componentDidMount() {
     if (!this.props.name) {
       this.props.history.push("./");
     }
+    fetch(`http://localhost:3000/users/${this.props.id}`)
+      .then(resp => resp.json())
+      .then(user => {
+        if (this.props.typeOfUser === "teacher") {
+          this.setState({
+            messages: user.my_messages,
+            bookings: user.my_bookings,
+            reviews: user.reviews,
+            students: user.students
+          });
+        } else {
+        }
+      });
   }
   render() {
     return (
