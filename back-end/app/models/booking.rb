e8f753722ea64b415a9d93417d17103702b2b9ae
DarkScarbo <xabier_abia@hotmail.com>
2019-09-09 17:25:22 +0100

    validations for booking model

--- back-end/app/models/booking.rb
+++ back-end/app/models/booking.rb
@@ -2,6 +2,10 @@ class Booking < ApplicationRecord
   validates :date, presence: true
   validates :starting, presence: true
   validates :ending, presence: true
+  validates :ending, presence: true
+  validates :student_name, presence: true
+  validates :student_email, presence: true
+
   belongs_to :student, foreign_key: 'student_id', class_name: 'User'
   belongs_to :teacher, foreign_key: 'teacher_id', class_name: 'User'
 
