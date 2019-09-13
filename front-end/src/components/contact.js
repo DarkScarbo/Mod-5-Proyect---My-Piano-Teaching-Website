f3fce373d32dd8a63d67126350a17053b91082c7
DarkScarbo <xabier_abia@hotmail.com>
2019-09-13 16:05:51 +0100

    More styling

--- front-end/src/components/contact.js
+++ front-end/src/components/contact.js
@@ -18,7 +18,7 @@ const Contact = () => {
       >
         Contact
       </Header>
-      <div style={{ padding: "0.95em 0em" }}>
+      <div style={{ padding: "1.2em 0em" }}>
         <Image
           style={{ paddingLeft: "1px" }}
           src={require("../Images/xabi-piano-00.jpg")}
@@ -28,19 +28,19 @@ const Contact = () => {
         <Header as="h2" style={{ fontWeight: "normal" }} className="p-review">
           To get in touch
         </Header>
-        <p className="p-review">
+        <div className="p-review">
           Mobile phone:
           <p style={{ fontWeight: "normal" }}>07539 175 029 </p>
-        </p>
-        <p className="p-review">
+        </div>
+        <div className="p-review">
           Email:
           <p style={{ fontWeight: "normal" }}>xabi.casan.piano@gmail.com</p>
-        </p>
-        <p className="p-review">
+        </div>
+        <div className="p-review">
           Studio:
           <p style={{ fontWeight: "normal" }}>SE1 3GU, London Bridge.</p>
-        </p>
-
+          <p>-------------------------------------- </p>
+        </div>
         <p className="p-review">Sign up or log in for more content!</p>
       </div>
     </Container>
